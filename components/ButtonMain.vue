ef70b81150b682f9eb7f0d36edc435d1576de7e0
Curtis Nielsen <14912039+curtisnn@users.noreply.github.com>
2022-09-14 12:05:35 -0700

    update hero section

---
+++ components/ButtonMain.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+	setup() {
+		return {};
+	},
+};
+</script>
+<template>
+	<button
+		type="button"
+		class="inline-flex items-center rounded-sm bg-ghxGold-500 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-ghxGold-400"
+	>
+		Button text
+		<!-- Heroicon name: mini/envelope -->
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="w-6 h-6"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M8.25 4.5l7.5 7.5-7.5 7.5"
+			/>
+		</svg>
+	</button>
+</template>
---
+++ components/SectionHero.vue
@@ -0,0 +1,49 @@
+<script>
+import ButtonMain from "@/components/ButtonMain.vue";
+export default {
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
+<template>
+	<!-- This example requires Tailwind CSS v2.0+ -->
+	<div class="bg-gray-50">
+		<div class="relative overflow-hidden">
+			<div class="absolute inset-y-0 h-full w-full" aria-hidden="true">
+				<div class="relative h-full">
+					<img
+						src="~/assets/images/HeroImage.png"
+						alt="Avatar"
+						class="object-cover w-full h-full"
+					/>
+				</div>
+			</div>
+
+			<div class="relative">
+				<div class="mx-auto max-w-7xl">
+					<div class="text-left font-roboto w-1/2 p-20 space-y-4">
+						<h6 class="text-xl tracking-tight text-gray-900">
+							Hospital Vendor Credentialing
+						</h6>
+						<h3
+							class="text-3xl tracking-tight text-gray-900 sm:text-4xl md:text-4xl"
+						>
+							Directory for reaching healthcare customers fast
+						</h3>
+						<p>
+							Trying to keep up with which hospital uses which
+							vendor credentialing program, who the right contacts
+							are. Now in one place, you can find all of this
+							information for your hospital customers and log
+							right in.
+						</p>
+						<ButtonMain />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
